d138edd433c74830e1733d4b2c0380df2ac92d8e
thisisyoursus <114288684+thisisyoursus@users.noreply.github.com>
2024-03-12 18:16:08 +0900

    Update EventHandlers.cs

--- CoinFlipTeleport/NoTrollBombs/EventHandlers.cs
+++ CoinFlipTeleport/NoTrollBombs/EventHandlers.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: NoTrollBombs.EventHandlers
-// Assembly: NoTrollBombs, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 74BF47E9-8C35-4DDA-905B-C26415F827F8
-// Assembly location: C:\Users\user\AppData\Local\Temp\tmp-33740-d42la7NR9er9\CoinFlipTeleport.dll
-
 using Exiled.API.Features;
 using Exiled.Events.EventArgs.Player;
 
