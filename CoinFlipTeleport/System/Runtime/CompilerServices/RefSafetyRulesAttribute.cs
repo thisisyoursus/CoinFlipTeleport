08142d1f5aaa4524543e364d12efa5275b4d8a6d
thisisyoursus <114288684+thisisyoursus@users.noreply.github.com>
2024-03-12 18:16:32 +0900

    Update RefSafetyRulesAttribute.cs

--- CoinFlipTeleport/System/Runtime/CompilerServices/RefSafetyRulesAttribute.cs
+++ CoinFlipTeleport/System/Runtime/CompilerServices/RefSafetyRulesAttribute.cs
@@ -1,9 +1,3 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: System.Runtime.CompilerServices.RefSafetyRulesAttribute
-// Assembly: NoTrollBombs, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 74BF47E9-8C35-4DDA-905B-C26415F827F8
-// Assembly location: C:\Users\user\AppData\Local\Temp\tmp-33740-d42la7NR9er9\CoinFlipTeleport.dll
-
 using Microsoft.CodeAnalysis;
 using System.Runtime.InteropServices;
 
