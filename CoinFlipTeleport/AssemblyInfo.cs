cf3e7de063ad9a0a77663d5e019963f42913743b
thisisyoursus <114288684+thisisyoursus@users.noreply.github.com>
2024-04-11 15:10:17 +0900

    Delete CoinFlipTeleport directory

--- CoinFlipTeleport/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("NoTrollBombs")]
-[assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyProduct("NoTrollBombs")]
-[assembly: AssemblyTitle("NoTrollBombs")]
-[assembly: AssemblyVersion("1.0.0.0")]
