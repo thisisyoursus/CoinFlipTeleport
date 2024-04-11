5baecb551476cee5714c0c4536f6170a557d73d8
thisisyoursus <114288684+thisisyoursus@users.noreply.github.com>
2024-04-11 15:14:44 +0900

    Add files via upload

---
+++ CoinFlipTeleport/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("NoTrollBombs")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("NoTrollBombs")]
+[assembly: AssemblyTitle("NoTrollBombs")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ CoinFlipTeleport/plugins/Config.cs
@@ -0,0 +1,68 @@
+using Exiled.API.Enums;
+using System.Collections.Generic;
+using Exiled.API.Interfaces;
+
+namespace CoinFlipTeleport
+{
+  public class Config : IConfig
+  {
+    public bool IsEnabled { get; set; } = true;
+
+    public bool Debug { get; set; } = false;
+
+        public HashSet<RoomType> RoomsToTeleport { get; set; } = new HashSet<RoomType>()
+        {
+            RoomType.EzCafeteria,
+            RoomType.EzCheckpointHallway,
+            RoomType.EzConference,
+            RoomType.EzCrossing,
+            RoomType.EzCurve,
+            RoomType.EzDownstairsPcs,
+            RoomType.EzGateA,
+            RoomType.EzGateB,
+            RoomType.EzIntercom,
+            RoomType.EzPcs,
+            RoomType.EzStraight,
+            RoomType.EzTCross,
+            RoomType.EzUpstairsPcs,
+            RoomType.EzVent,
+            RoomType.Hcz049,
+            RoomType.Hcz079,
+            RoomType.Hcz096,
+            RoomType.Hcz106,
+            RoomType.Hcz939,
+            RoomType.HczArmory,
+            RoomType.HczCrossing,
+            RoomType.HczCurve,
+            RoomType.HczElevatorA,
+            RoomType.HczElevatorB,
+            RoomType.HczEzCheckpointA,
+            RoomType.HczEzCheckpointB,
+            RoomType.HczHid,
+            RoomType.HczNuke,
+            RoomType.HczServers,
+            RoomType.HczStraight,
+            RoomType.HczTCross,
+            RoomType.HczTesla,
+            RoomType.HczTestRoom,
+            RoomType.Lcz173,
+            RoomType.Lcz330,
+            RoomType.Lcz914,
+            RoomType.LczAirlock,
+            RoomType.LczArmory,
+            RoomType.LczCafe,
+            RoomType.LczCheckpointA,
+            RoomType.LczCheckpointB,
+            RoomType.LczClassDSpawn,
+            RoomType.LczCrossing,
+            RoomType.LczCurve,
+            RoomType.LczGlassBox,
+            RoomType.LczPlants,
+            RoomType.LczStraight,
+            RoomType.LczTCross,
+            RoomType.LczToilets,
+            RoomType.Surface,
+        };
+
+    }
+}
